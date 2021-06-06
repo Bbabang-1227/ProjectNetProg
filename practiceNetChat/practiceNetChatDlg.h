6369f3b5b2b9e17cc6d837d88b8129c8e37a4a71
Bbabang-1227 <83930052+Bbabang-1227@users.noreply.github.com>
2021-06-06 15:50:27 +0900

    practice
    1

---
+++ practiceNetChat/practiceNetChatDlg.h
@@ -0,0 +1,38 @@
+﻿
+// practiceNetChatDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CpracticeNetChatDlg 대화 상자
+class CpracticeNetChatDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CpracticeNetChatDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PRACTICENETCHAT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListBox m_ctrList;
+	afx_msg void OnBnClickedButtonEnter();
+	afx_msg void OnBnClickedButtonSend();
+};
